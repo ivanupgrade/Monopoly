69a8768cd6f7565f07434405c7a78526f91c8da9
Marta Sánchez-Tornero <marta.tornero@bootcamp-upgrade.com>
2025-05-27 18:32:31 +0200

    Terminado actualizarExistenete en DAO de partida

--- MONOPOLY/src/main/java/DAO/PartidaDAOImpl.java
+++ MONOPOLY/src/main/java/DAO/PartidaDAOImpl.java
@@ -90,7 +90,7 @@ public class PartidaDAOImpl implements PartidaDAO{
     public Partida obtenerExistente (int id){
         String sql = "SELECT * FROM partidas WHERE id = ?";
         String sql2 = "SELECT * FROM cartas_partidas LEFT JOIN cartas ON id = id_cartas where id_partidas = ?";
-        String sql3 = "SELECT * FROM jugadores_partidas LEFT JOIN jugadores ON id = id_jugadores where id_partidas = ?";
+        String sql3 = "SELECT * FROM partidas_jugadores LEFT JOIN jugadores ON id = id_jugadores where id_partidas = ?";
         String sql4 = """
                 SELECT * FROM partidas_jugadores_calles LEFT JOIN calles ON partidas_jugadores_calles.posicion = calles.posicion
                 LEFT JOIN jugadores ON id_jugadores = jugadores.id
@@ -199,5 +199,76 @@ public class PartidaDAOImpl implements PartidaDAO{
             throw new RuntimeException(e);
         }
         return null;
+    }
+
+
+    public void actualizarExistente(Partida partida) {
+
+        String sql = "UPDATE partidas SET fecha = curdate(), turno = ? WHERE id = ?";
+        String sql2 = "UPDATE cartas_partidas SET baraja = ? WHERE carta_id = ? AND partida_id = ?";
+        String sql3 = "UPDATE partidas_jugadores SET encarcelado = ?, dinero = ?, posicion = ? WHERE jugador_id = ? AND partida_id = ?";
+        String sql4 = "UPDATE partidas_jugadores_calles SET n_casas = ?, alquiler_mod = ?, jugador_id = ? WHERE partida_id = ? AND posicion = ?";
+
+        try (
+                PreparedStatement pst = conn.prepareStatement(sql);
+                PreparedStatement pst2 = conn.prepareStatement(sql2);
+                PreparedStatement pst3 = conn.prepareStatement(sql3);
+                PreparedStatement pst4 = conn.prepareStatement(sql4)
+        ){
+            //sql
+            pst.setInt(1,partida.getTurno());
+            pst.setInt(2,partida.getId());
+
+            //sql2
+            for (CartaSuerte carta : partida.getMazoRobo().getBaraja()){
+                pst2.setInt(1,0);
+                pst2.setInt(2,carta.getId());
+                pst2.setInt(3,partida.getId());
+                pst2.addBatch();
+            }
+
+            for (CartaSuerte carta : partida.getMazoDescarte().getBaraja()){
+                pst2.setInt(1,1);
+                pst2.setInt(2,carta.getId());
+                pst2.setInt(3,partida.getId());
+                pst2.addBatch();
+            }
+
+            //sql3
+            for (Jugador jugador : partida.getJugadores()){
+                pst3.setBoolean(1,jugador.isEncarcelado());
+                pst3.setInt(2, jugador.getDinero());
+                pst3.setInt(3,jugador.getPosicion());
+                pst3.setInt(4, jugador.getId());
+                pst3.setInt(5, partida.getId());
+                pst3.addBatch();
+            }
+
+            //sql4 String sql4 = "UPDATE partidas_jugadores_calles SET n_casas = ?, alquiler_mod = ?, jugador_id = ? WHERE partida_id = ? AND posicion = ?";
+            for (Jugador jugador : partida.getJugadores()){
+                if (!jugador.getCalles().isEmpty()){
+                    for (Calle calle : jugador.getCalles()){
+                        if (calle instanceof Normal){
+                            pst4.setInt(1, ((Normal) calle).getNumCasas());
+                        }else {
+                            pst4.setInt(1,0);
+                        }
+                        pst4.setInt(2,calle.getAlquiler());
+                        pst4.setInt(3,jugador.getId());
+                        pst4.setInt(4,partida.getId());
+                        pst4.setInt(5,calle.getPosicion());
+                        pst4.addBatch();
+                    }
+                }
+            }
+
+            pst.executeUpdate();
+            pst2.executeBatch();
+            pst3.executeBatch();
+            pst4.executeBatch();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
